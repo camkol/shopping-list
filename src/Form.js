8a7c042abf1ce74466439c4596b31929867ef57b
camkol <koliyus3@hotmail.com>
2024-05-18 06:44:50 +0200

    setting up

---
+++ src/Form.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+export default function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = {
+      description,
+      quantity,
+      amount,
+      packed: false,
+      id: Date.now(),
+    };
+    //console.log(newItem);
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setAmount("");
+    setQuantity(1);
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What groceries do you need?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <input
+        className="amount"
+        type="text"
+        placeholder="Amount..."
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
